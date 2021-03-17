892ea89dcc0f1530628dd45f84c9111a688aaef4
Vitalii Shvetsov <vitalya199529@gmail.com>
2021-03-17 09:03:58 +0900

    Clean project and define Todo type.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TodoListItem } from './TodoListItem';
+
+const todos: Todo[] = [
+  {
+    timestamp: new Date().toUTCString(),
+    text: 'aaaaaaa',
+    complete: true,
+  },
+  {
+    timestamp: new Date().toUTCString(),
+    text: 'bbbbbb',
+    complete: false,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <TodoListItem todo={todos[0]} />
+      <TodoListItem todo={todos[1]} />
+    </ul>
   );
 }
 
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+interface Todo {
+    timestamp: string;
+    text: string;
+    complete: boolean;
+}
